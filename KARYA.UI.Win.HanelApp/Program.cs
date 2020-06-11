34b4fc9b975ff0a313523ac1bc15dab4386c6574
Ramazan Karagoz <ramazankaragoz@outlook.com>
2020-06-11 13:07:17 +0300

    First commit

--- KARYA.UI.Win.HanelApp/Program.cs
+++ KARYA.UI.Win.HanelApp/Program.cs
@@ -14,6 +14,8 @@ namespace KARYA.HanelApp.UI.Win
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        /// //dsflsdiflsdlf
+        /// 
         [STAThread]
         static void Main()
         {
